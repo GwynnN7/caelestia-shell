c88734e0016b4bdda0cc72e678d541ffbbd81ac6
GwynnN7 <mattcheru03@gmail.com>
2026-05-10 17:11:54 +0200

    Test

--- plugin/src/Caelestia/Config/serviceconfig.hpp
+++ plugin/src/Caelestia/Config/serviceconfig.hpp
@@ -29,11 +29,13 @@ class ServiceConfig : public ConfigObject {
     CONFIG_GLOBAL_PROPERTY(qreal, brightnessIncrement, 0.1)
     CONFIG_GLOBAL_PROPERTY(qreal, maxVolume, 1.0)
     CONFIG_GLOBAL_PROPERTY(bool, smartScheme, true)
-    CONFIG_GLOBAL_PROPERTY(QString, defaultPlayer, u"Spotify"_s)
-    CONFIG_GLOBAL_PROPERTY(QVariantList, playerAliases,
-        { vmap({ { u"from"_s, u"com.github.th_ch.youtube_music"_s }, { u"to"_s, u"YT Music"_s } }) })
-    CONFIG_GLOBAL_PROPERTY(bool, showLyrics, false)
-    CONFIG_GLOBAL_PROPERTY(QString, lyricsBackend, u"Auto"_s)
+    CONFIG_GLOBAL_PROPERTY(QString, defaultPlayer, u"Tidal"_s)
+    CONFIG_GLOBAL_PROPERTY(QVariantList, playerAliases, {
+        vmap({ { u"from"_s, u"com.github.th_ch.youtube_music"_s }, { u"to"_s, u"YT Music"_s } }),
+        vmap({ { u"from"_s, u"tidal-hifi"_s }, { u"to"_s, u"Tidal"_s } })
+    })
+    CONFIG_GLOBAL_PROPERTY(bool, showLyrics, true)
+    CONFIG_GLOBAL_PROPERTY(QString, lyricsBackend, u"NetEase"_s)
 
 public:
     explicit ServiceConfig(QObject* parent = nullptr)
